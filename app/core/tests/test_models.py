492691fcd266216f6378b4bb96c1f9f0f503c459
Almas M <kramal888@gmail.com>
2020-12-11 15:56:00 +0600

    added working list tags endpoint

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+from core import models
+
 
 def sample_user(email='test@mail.kz', password='testpasss'):
     """create a sample user"""
